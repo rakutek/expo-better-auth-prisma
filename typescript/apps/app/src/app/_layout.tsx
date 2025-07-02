6a8a49d8ba9248fb9eb2981db5afa5b84d640768
rakutek <rakutek12@gmail.com>
2025-07-03 00:23:00 +0900

    Refactor code structure and remove redundant sections for improved readability and maintainability

---
+++ typescript/apps/app/src/app/_layout.tsx
@@ -0,0 +1,41 @@
+import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native'
+import { useFonts } from 'expo-font'
+import { Stack } from 'expo-router'
+import { StatusBar } from 'expo-status-bar'
+import 'react-native-reanimated'
+
+import { useColorScheme } from '@/hooks/useColorScheme'
+import { authClient } from '@/lib/auth-client'
+
+export default function RootLayout() {
+  const colorScheme = useColorScheme()
+  const [loaded] = useFonts({
+    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+  })
+  const { data: session, isPending } = authClient.useSession()
+  const isAuthenticated = !!(session && session.user && session.user.id)
+
+  if (!loaded || isPending) {
+    // Async font loading only occurs in development.
+    return null
+  }
+
+  return (
+    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+      <Stack>
+        <Stack.Protected guard={isAuthenticated}>
+          <Stack.Screen
+            name="(tabs)"
+            options={{ headerShown: false }}
+          />
+
+        </Stack.Protected>
+        <Stack.Screen
+          name="loginPage"
+          options={{ headerShown: false }}
+        />
+      </Stack>
+      <StatusBar style="auto" />
+    </ThemeProvider>
+  )
+}
